4e2173a01c0ed977f3c585da8c59db8e0bfe4045
Chiranjit Karmakar <62233288+chiranjit2020@users.noreply.github.com>
2022-03-25 20:07:58 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const input = document.getElementById('text');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', func);
+
+function func(e){
+	e.preventDefault;
+	alert('hi');
+}
